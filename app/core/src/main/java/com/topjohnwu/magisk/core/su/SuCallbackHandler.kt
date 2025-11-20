8906a40a8e58e30100ac297e12ddafd3b8a11aa8
EdgeAtZero <edgeatzero@gmail.com>
2025-11-20 15:38:41 +0800

    fallback some files

--- app/core/src/main/java/com/topjohnwu/magisk/core/su/SuCallbackHandler.kt
+++ app/core/src/main/java/com/topjohnwu/magisk/core/su/SuCallbackHandler.kt
@@ -29,7 +29,6 @@ object SuCallbackHandler {
             Timber.d(action)
             data.let { bundle ->
                 bundle.keySet().forEach {
-                    @Suppress("DEPRECATION")
                     Timber.d("[%s]=[%s]", it, bundle[it])
                 }
             }
@@ -43,7 +42,6 @@ object SuCallbackHandler {
 
     // https://android.googlesource.com/platform/frameworks/base/+/547bf5487d52b93c9fe183aa6d56459c170b17a4
     private fun Bundle.getIntComp(key: String, defaultValue: Int): Int {
-        @Suppress("DEPRECATION")
         val value = get(key) ?: return defaultValue
         return when (value) {
             is Int -> value
